16a7f84c726462c7283e8779e9dfdddbcb003cb1
RomainP01 <romain.pani@decathlon.com>
2022-10-05 09:13:17 +0200

    [ALGOTRAINING] - Initializing project

---
+++ src/main/java/com/example/RomainP01/algorithmstraining/AlgorithmsTrainingApplication.java
@@ -0,0 +1,17 @@
+package com.example.RomainP01.algorithmstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsTrainingApplication.class, args);
+	}
+
+	public void algo1(){
+
+	}
+
+}
