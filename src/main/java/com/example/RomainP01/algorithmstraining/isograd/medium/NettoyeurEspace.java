c3f8d90306d6b353e279f9f6b67383d0512972c0
RomainP01 <romain.pani@decathlon.com>
2022-10-12 17:45:44 +0200

    [ALGOTRAINING] - wip

--- src/main/java/com/example/RomainP01/algorithmstraining/isograd/medium/NettoyeurEspace.java
+++ src/main/java/com/example/RomainP01/algorithmstraining/isograd/medium/NettoyeurEspace.java
@@ -1,20 +1,47 @@
 package com.example.RomainP01.algorithmstraining.isograd.medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class NettoyeurEspace {
-    public static void main( String[] argv ) throws Exception {
+    public static String sortString(String inputString) {
+        // Converting input string to character array
+        char tempArray[] = inputString.toCharArray();
+
+        // Sorting temp array using
+        Arrays.sort(tempArray);
+
+        // Returning new sorted string
+        return new String(tempArray);
+    }
+
+    public static void main(String[] argv) throws Exception {
         Scanner sc = new Scanner(System.in);
+        var firstLine = sc.nextLine();
         var garbages = sc.nextLine().split("");
-        var numOfWays =0;
-        Map<String,Integer> allLetters =new HashMap<>() ;
-        for(String g:garbages){
-            allLetters.computeIfPresent(g,(k,v)->v+1);
-            allLetters.computeIfAbsent(g,k->allLetters.put(k,1));
+        var boolean =Arrays.stream(garbages).anyMatch("2");
+        var numOfWays = 0;
+        for (var i = 0; i < garbages.length; i++) {
+            var robot1 = "";
+            var robot2 = "";
+            var k = 0;
+            while (k < garbages.length) {
+                if (k + i < garbages.length) {
+                    robot1 += garbages[k + i];
+                } else {
+                    robot1 += garbages[(k + i) - (garbages.length - 1)];
+                }
+                if (k + i + garbages.length / 2 < garbages.length) {
+                    robot1 += garbages[k + i + garbages.length / 2];
+                } else {
+                    robot1 += garbages[(k + i + garbages.length / 2) - (garbages.length - 1)];
+                }
+                k += 1;
+            }
+            if (sortString(robot1).equals(sortString(robot2))) {
+                numOfWays += 2;
+            }
         }
+        System.out.println(numOfWays);
 
     }
 }
