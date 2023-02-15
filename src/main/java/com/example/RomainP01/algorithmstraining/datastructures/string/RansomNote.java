f7347ac1b430c57b927e340576ed65342776e39b
RomainP01 <romain.pani@decathlon.com>
2023-02-15 10:46:54 +0100

    LEETCODE(ransomnote) - adding explanation

--- src/main/java/com/example/RomainP01/algorithmstraining/datastructures/string/RansomNote.java
+++ src/main/java/com/example/RomainP01/algorithmstraining/datastructures/string/RansomNote.java
@@ -3,13 +3,17 @@ package com.example.RomainP01.algorithmstraining.datastructures.string;
 public class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
+        //count the number of each letter in magazine
         for(int i=0; i<magazine.length(); i++){
+            //count[0] = count['a'-'a'] = count[0] + 1
             count[magazine.charAt(i)-'a']++;
         }
         for(int i=0; i<ransomNote.length(); i++){
+            //if the letter in ransomNote is not in magazine, return false
             if(count[ransomNote.charAt(i)-'a']<=0){
                 return false;
             }else{
+                //if the letter in ransomNote is in magazine, decrease the count
                 count[ransomNote.charAt(i)-'a']--;
             }
         }
