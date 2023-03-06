225f1407e130d753064177c08db7bc012db88f4a
Romain <romainpani01@gmail.com>
2023-03-06 09:38:29 +0100

    feat(leetcode) tree

---
+++ src/main/java/com/example/RomainP01/algorithmstraining/datastructures/tree/SymmetricTree.java
@@ -0,0 +1,22 @@
+package com.example.RomainP01.algorithmstraining.datastructures.tree;
+
+import com.example.RomainP01.algorithmstraining.model.TreeNode;
+
+public class SymmetricTree {
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+}
